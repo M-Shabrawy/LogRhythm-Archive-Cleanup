02e18ecf0064d56586532108512f233602db68a1
Mohamed Al-Shabrawy <12400622+M-Shabrawy@users.noreply.github.com>
2018-08-14 12:56:41 +0300

    Add files via upload

---
+++ LR_Archive_Cleanup.ps1
@@ -0,0 +1,27 @@
+#LogRhythm Archive Clenaup
+
+#Archive Path
+$Path = "D:\LogRhythmArchives\Inactive"
+
+#Log File
+$LogFile = "D:\LogRhythm\ArchiveCleanup.log"
+
+#Number of days to keep
+$Days = 3 * -365
+
+$LastDay = (Get-Date).AddDays($Days)
+
+$Today = (Get-Date)
+
+$Archives = Get-ChildItem -Path $Path -Directory
+foreach ($Archive in $Archives)
+{
+    #Write-Host ($Archive.BaseName).Substring(4,2)"/"($Archive.BaseName).Substring(6,2)
+    $ArchDay = [DateTime](($Archive.BaseName).Substring(0,4)+"/"+($Archive.BaseName).Substring(4,2)+"/"+($Archive.BaseName).Substring(6,2))
+    if ($ArchDay -lt $LastDay)
+    {
+        Remove-Item -Path $Archive.FullName -Recurse -Force
+        "Archive Folder " + $Archive.BaseName + " Deleted" | Out-File -FilePath $LogFile -Append -Force 
+    }
+}
+exit 0
