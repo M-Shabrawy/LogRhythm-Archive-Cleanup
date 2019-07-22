2656b7e7e96dbb5bbaa2a47e8e9484575b2a65a0
Mohamed Al-Shabrawy <12400622+M-Shabrawy@users.noreply.github.com>
2019-07-22 09:50:40 +0300

    Update LR_Archive_Cleanup.ps1

--- LR_Archive_Cleanup.ps1
+++ LR_Archive_Cleanup.ps1
@@ -1,33 +1,46 @@
 #LogRhythm Archive Clenaup
 
+param(
+    #Inactive Archive Path
+    [string]$Path,
+    [int]$Years = 3,
+    [bool]$Test = $false
+)
+
 trap [Exception] 
 {
 	write-error $("Exception: " + $_)
 	exit 1
 }
 
-#Archive Path
-$Path = "D:\LogRhythmArchives\Inactive"
+if ($Path -eq "")
+{
+    $Path = "D:\LogRhythmArchives\Inactive"
+}
 
 #Log File
-$LogFile = "D:\LogRhythm\ArchiveCleanup.log"
+$LogFile = "D:\LogRhythmArchives\ArchiveCleanup.log"
 
 #Number of days to keep
-$Days = 3 * -365
+$Days = $Years * -365
 
-$LastDay = (Get-Date).AddDays($Days)
+$LastDay = (Get-Date).AddDays($Days).ToString("yyyMMdd")
 
-$Today = (Get-Date)
-
-$Archives = Get-ChildItem -Path $Path -Directory
+$Archives = Get-ChildItem -Path $Path -Directory -Recurse -Depth 1 | Where-Object{ $_.Name -Match "\d{6}_\d+_\d+.*"}
 foreach ($Archive in $Archives)
 {
-    #Write-Host ($Archive.BaseName).Substring(4,2)"/"($Archive.BaseName).Substring(6,2)
-    $ArchDay = [DateTime](($Archive.BaseName).Substring(0,4)+"/"+($Archive.BaseName).Substring(4,2)+"/"+($Archive.BaseName).Substring(6,2))
+    $ArchDay = ($Archive.BaseName).Substring(0,8)
     if ($ArchDay -lt $LastDay)
     {
-        Remove-Item -Path $Archive.FullName -Recurse -Force
-        "Archive Folder " + $Archive.BaseName + " Deleted" | Out-File -FilePath $LogFile -Append -Force 
+        if ($Test)
+        {
+            Write-Host "Archive Folder " + $Archive.BaseName + " will be deleted"
+        }
+        else
+        {
+           Remove-Item -Path $Archive.FullName -Recurse -Force
+           "Archive Folder " + $Archive.BaseName + " Deleted" | Out-File -FilePath $LogFile -Append -Force
+        }
     }
 }
 exit 0
